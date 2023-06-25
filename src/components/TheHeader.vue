3f91711cb5e9fc675021efeec3deb85134ee0b3e
MoisesST <moises.dev13@gmail.com>
2023-06-25 17:48:40 -0300

    feat: instrucoes de loops (diretiva v-for)

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,62 @@
 <template>
-  <header class="header">
-    Header
-  </header>
+  <div>
+    <div
+      v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+      class="todos-item"
+    >
+     {{ index }} - {{ obj.title }}
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        todos: [
+          {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+          }
+        ]
+      }
+    }
   }
 </script>
 
 <style>
-  .header {
-    background: #000;
+  .todos-item {
+    margin: 0 0 5px 0;
+    padding: 3px 6px;
+    text-align: start;
     color: #fff;
+    background: #000;
   }
 </style>
