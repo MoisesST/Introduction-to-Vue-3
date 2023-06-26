4ebf47e05e38a3a953512db4d609511a0f15e46b
MoisesST <moises.dev13@gmail.com>
2023-06-25 22:35:34 -0300

    feat: eventos

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
 
   <TheForm />
 
+  <TheEvents />
+
   <div v-show="showName">
     Name: {{ firstName }} <br>
     Last Name: {{ lastName }}
@@ -21,14 +23,15 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheHeader from './components/TheHeader.vue'
 import TheForm from './components/TheForm.vue'
-
+import TheEvents from './components/TheEvents.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     TheHeader,
-    TheForm
+    TheForm,
+    TheEvents
   },
   data() {
     return {
@@ -47,7 +50,6 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
     color: #2c3e50;
     margin-top: 60px;
   }
---
+++ src/components/TheEvents.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click.once="onClick">
+      Send1
+    </button> <br><br>
+    
+    <div @mouseover="onMouseOver">
+      Mouse over
+    </div> <br>
+
+    <form 
+      action="https://google.com"
+      @submit.prevent="onSubmit"
+    >
+      <input 
+        type="text"
+        @keyup="onKeyUp"
+      >
+      <button>Send2</button> 
+    </form>
+
+    <br><br><br>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      onClick(e) {
+        alert('click')
+        console.log(e)
+      },
+      onMouseOver() {
+        alert('mouse over')
+      },
+      onSubmit() {
+        console.log('submit')
+      },
+      onKeyUp(e) {
+        console.log('onKeyUp', e)
+      }
+    }
+  }
+</script>
+
+<style></style>
